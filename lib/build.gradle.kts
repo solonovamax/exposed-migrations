91c9c93f56716c7107964a96e243950c24365710
solonovamax <solonovamax@12oclockpoint.com>
2021-09-01 23:53:41 -0400

    Use org.reflections and slf4k libraries

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -11,30 +11,30 @@ repositories {
 dependencies {
     // Align versions of all Kotlin components
     compileOnly(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
-    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
+    
     // Logging
     compileOnly("org.slf4j:slf4j-api:1.7.30")
-    compileOnly("ch.qos.logback:logback-classic:1.2.3")
-    compileOnly("io.github.microutils:kotlin-logging:2.0.6")
-
+    
+    implementation("ca.solo-studios:slf4k:0.3.1")
+    
+    api("org.reflections:reflections:0.9.12")
+    
     // Exposed ORM
     val exposedVersion = "0.31.1"
-    compileOnly("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    compileOnly("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-    compileOnly("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-    compileOnly("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
-
+    api("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    api("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    api("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    api("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+    
 }
 
 tasks {
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {
+            jvmTarget = "8"
             freeCompilerArgs = listOf(
-                    "-Xopt-in=kotlin.RequiresOptIn",
-            )
+                "-Xopt-in=kotlin.RequiresOptIn",
+                                     )
         }
     }
 }
@@ -45,22 +45,22 @@ java {
 
 publishing {
     val libVersion = "3.1.1"
-
+    
     publications {
         create<MavenPublication>("defaultJar") {
             groupId = "com.github.suwayomi"
             artifactId = "exposed-migrations"
             version = libVersion
-
+            
             from(components["kotlin"])
         }
-
+        
         create<MavenPublication>("sourcesJar") {
             groupId = "com.github.suwayomi"
             artifactId = "exposed-migrations"
             version = "$libVersion-sources"
             artifact(tasks.named("sourcesJar"))
-
+            
             from(components["kotlin"])
         }
     }
--- lib/src/main/kotlin/de/neonew/exposed/migrations/migrations.kt
+++ lib/src/main/kotlin/de/neonew/exposed/migrations/migrations.kt
@@ -1,40 +1,40 @@
 package de.neonew.exposed.migrations
 
-import mu.KotlinLogging
+import java.time.Clock
+import java.time.Instant.now
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils.create
 import org.jetbrains.exposed.sql.exists
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.nio.file.FileSystems
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.time.Clock
-import java.time.Instant.now
-import kotlin.io.path.ExperimentalPathApi
-import kotlin.io.path.isDirectory
-import kotlin.io.path.name
-import kotlin.streams.toList
+import org.reflections.Reflections
+import org.slf4j.kotlin.debug
+import org.slf4j.kotlin.getLogger
+import org.slf4j.kotlin.info
 
-private val logger = KotlinLogging.logger {}
+private val logger by getLogger()
 
 internal lateinit var migrationsDatabase: Database
 
-fun runMigrations(migrations: List<Migration>, database: Database = TransactionManager.defaultDatabase!!, clock: Clock = Clock.systemUTC()) {
+fun runMigrations(
+    migrations: List<Migration>,
+    database: Database = TransactionManager.defaultDatabase!!,
+    clock: Clock = Clock.systemUTC()
+                 ) {
     migrationsDatabase = database
-
+    
     checkVersions(migrations)
-
+    
     logger.info { "Running migrations on database ${database.url}" }
-
+    
     val latestVersion = transaction(database) {
         createTableIfNotExists(database)
         MigrationEntity.all().maxByOrNull { it.version }?.version?.value
     }
-
+    
     logger.info { "Database version before migrations: $latestVersion" }
-
+    
     migrations
         .sortedBy { it.version }
         .filter { shouldRun(latestVersion, it) }
@@ -42,7 +42,7 @@ fun runMigrations(migrations: List<Migration>, database: Database = TransactionM
             logger.info { "Running migration version ${it.version}: ${it.name}" }
             transaction(database) {
                 it.run()
-
+                
                 MigrationEntity.new {
                     version = EntityID(it.version, MigrationsTable)
                     name = it.name
@@ -50,45 +50,19 @@ fun runMigrations(migrations: List<Migration>, database: Database = TransactionM
                 }
             }
         }
-
+    
     logger.info { "Migrations finished successfully" }
 }
 
-@OptIn(ExperimentalPathApi::class)
-private fun getTopLevelClasses(packageName: String, klass: Class<*>): List<Class<*>> {
-    klass.getResource("/" + packageName.replace('.', '/'))
-    val path = "/" + packageName.replace('.', '/')
-    val uri = klass.getResource(path).toURI()
-
-    return when (uri.scheme) {
-        "jar" -> {
-            val fileSystem = FileSystems.newFileSystem(uri, emptyMap<String, Any>())
-            fileSystem.getPath(path)
-        }
-        else -> Paths.get(uri)
-    }.let { Files.walk(it, 1) }
-        .toList()
-        .filterNot { it.isDirectory() || it.name.contains('$') } // '$' means it's not a top level class
-        .filter { it.name.endsWith(".class") }
-        .map { Class.forName("$packageName.${it.name.substringBefore(".class")}") }
-}
-
-@Suppress("UnstableApiUsage")
-fun loadMigrationsFrom(packageName: String, klass: Class<*>): List<Migration> {
-    return getTopLevelClasses(packageName, klass)
-        .map {
-            logger.debug("found Migration class ${it.name}")
-            val clazz = it.getDeclaredConstructor().newInstance()
-            if (clazz is Migration)
-                clazz
-            else
-                throw RuntimeException("found a class that's not a Migration")
-        }
+private fun getTopLevelClasses(packageName: String, klass: Class<*>): Set<Class<*>> {
+    val reflections = Reflections(packageName)
+    
+    return reflections.getSubTypesOf(Migration::class.java)
 }
 
 private fun checkVersions(migrations: List<Migration>) {
     val sorted = migrations.map { it.version }.sorted()
-    if ((1..migrations.size).toList() != sorted) {
+    if ((1 .. migrations.size).toList() != sorted) {
         throw IllegalStateException("List of migrations version is not consecutive: $sorted")
     }
 }
@@ -99,10 +73,11 @@ private fun createTableIfNotExists(database: Database) {
     }
     val tableNames = database.dialect.allTablesNames()
     when (tableNames.isEmpty()) {
-        true -> {
+        true  -> {
             logger.info { "Empty database found, creating table for migrations" }
             create(MigrationsTable)
         }
+        
         false -> throw IllegalStateException("Tried to run migrations against a non-empty database without a Migrations table. This is not supported.")
     }
 }
